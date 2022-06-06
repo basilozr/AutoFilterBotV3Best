01940fef53d78f6c509f7f4e841253c951cf49a7
basilozr <106648030+basilozr@users.noreply.github.com>
2022-06-06 12:23:24 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -115,7 +115,7 @@ async def start(bot, message):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[
-                InlineKeyboardButton("🔎 Search Here 🔍", switch_inline_query_current_chat='')
+                InlineKeyboardButton("🔎  Search Here  🔍", switch_inline_query_current_chat='')
                 ],[
                 InlineKeyboardButton("ℹ️ Help", callback_data="help"),
                 InlineKeyboardButton("📝 About", callback_data="about")
